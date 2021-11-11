8303306aff8115a38369e22cbfb8c31e538ea678
Wellsrodrigues <93782799+Wellsrodrigues@users.noreply.github.com>
2021-11-11 11:28:22 -0300

    Bem vindo

--- Q1.cpp
+++ Q1.cpp
@@ -1,20 +1,26 @@
-#include "stdio.h"
+#include <stdio.h>
 
-int main(){
-	char nome[45];
-	char sexo;
-	
-	printf("NOME: ");
-	scanf("%s", &nome);
-	
-	printf("SEXO: ");
-	scanf("%s", &sexo);
-	
-	
-	if(sexo == 'F' || sexo == 'f'){
-		printf("Bem-vinda %s!\n", nome);
-	}
-	else{
-		printf("Bem-vindo %s!\n", nome);
-	}
+/*Faça um programa que receba o nome e o sexo de uma pessoa, se for do sexo feminino, o
+programa irá dizer Bem-vinda + nome da pessoa, se for masculino, Bem-vindo + nome da
+pessoa. (IF-ELSE)*/
+
+int main(void) { 
+    char nome[50]; 
+     char sexo; 
+    
+    printf("Digite seu nome: "); 
+    scanf("%s", &nome);   
+          
+    printf("Informe sexo: ");   
+    scanf("%s", &sexo); 
+            
+    if (sexo == 'F' || sexo == 'f'){   
+     printf("Bem-vinda, %s\n", nome); 
+   }  
+   else if(sexo == 'M' || sexo == 'm'){   
+       printf("Bem-vindo, %s", nome);  
+   }
+   else{
+   	  printf("Sexo incorreto");
+   }
 }
